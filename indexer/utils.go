a7e3e171cc27996ce5cc659092fe6a2a1293300d
cherideal <101du@163.com>
2021-12-06 13:12:44 +0800

    debug

--- indexer/utils.go
+++ indexer/utils.go
@@ -226,10 +226,9 @@ func (p *psqlBackend) StoreBlockData(oasisBlock *block.Block, txResults []*clien
 		resEvents := item.Events
 		for eventIndex, event := range resEvents {
 			if event.Code == 1 {
-				p.logger.Debug("unmarshal event value", "value", event.Value)
 				var logs []*Log
 				if err = cbor.Unmarshal(event.Value, &logs); err != nil {
-					p.logger.Error("Failed to unmarshal event value", "index", eventIndex)
+					p.logger.Error("Failed to unmarshal event value", "index", eventIndex, "value", event.Value)
 					continue
 				}
 				oasisLogs = append(oasisLogs, logs...)
